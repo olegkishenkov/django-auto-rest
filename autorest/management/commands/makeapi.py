a3e976ec957a12e819af486a7a9ff086d91a64ed
oleg1248 <oleg1248163264@gmail.com>
2020-05-07 18:33:34 +0300

    made autorest actually render an automatic_urls.py file

--- autorest/management/commands/makeapi.py
+++ autorest/management/commands/makeapi.py
@@ -1,5 +1,9 @@
+import os
+
 import django.apps
+import django.conf
 from django.core.management import BaseCommand
+from django.template import Engine, Context
 
 
 class Command(BaseCommand):
@@ -14,7 +18,35 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         models = django.apps.apps.get_models()
+
         if not options['dry_run']:
-            pass
+            absolute_dir = os.path.join(django.conf.settings.BASE_DIR, 'autorest')
+            old_path = os.path.join(absolute_dir, 'management', 'commands', 'automatic_urls.py-tpl')
+            with open(old_path, encoding='utf-8') as template_file:
+                content = template_file.read()
+            template = Engine().from_string(content)
+            models_ = []
+            for model in models:
+                if model.__name__.lower().endswith('y'):
+                    lower_case_name_plural = model.__name__.lower().rstrip('y') + 'ies'
+                else:
+                    lower_case_name_plural = model.__name__.lower() + 's'
+                models_.append(
+                    {
+                        'name': model.__name__,
+                        'lower_case_name_plural': lower_case_name_plural,  # TODO: take into account -y endings
+                        'module': model.__module__,
+                        'fields': [field.name for field in model._meta.fields],
+                    }
+                )
+            context = Context({
+                **options,
+                'models': models_,
+            }, autoescape=False)
+
+            content = template.render(context)
+            new_path = os.path.join(absolute_dir, 'automatic_urls.py')
+            with open(new_path, 'w', encoding='utf-8') as new_file:
+                new_file.write(content)
         models_string = ' '.join([str(model.__name__) for model in models])
         self.stdout.write(self.style.SUCCESS('a REST API created for models {}'.format(models_string)))
--- mysite/urls.py
+++ mysite/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('mysite.automatic_urls')),
+    path('', include('autorest.automatic_urls')),
     path('admin/', admin.site.urls),
 ]
