81ae5c3fef008dec4618421d25cf10504b119b6e
oleg1248 <oleg1248163264@gmail.com>
2020-05-10 18:23:09 +0300

    added the autorest app that generates the necessary classes upon a HTTP request and publishes a REST API on the fly

---
+++ autorest/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AutorestConfig(AppConfig):
+    name = 'autorest'
---
+++ autorest/views.py
@@ -0,0 +1,70 @@
+import copy
+
+import django.apps
+from rest_framework import viewsets
+from rest_framework.exceptions import ValidationError
+from rest_framework.response import Response
+from rest_framework.serializers import HyperlinkedModelSerializer
+
+
+def _single_from_plural(s):
+    return s.rstrip('ies') + 'y' if s.endswith('ies') else s.rstrip('s')
+
+
+class GETparamsViewSet(viewsets.ModelViewSet):
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        query_params = copy.deepcopy(dict(self.request.query_params))
+        try:
+            order_by = query_params.pop('order_by')[0]
+        except KeyError:
+            order_by = None
+        try:
+            limit = int(query_params.pop('limit')[0])
+            if limit < 0:
+                limit = None
+        except (KeyError, ValueError):
+            limit = None
+        for key in query_params.keys():
+            query_params[key] = query_params[key][0]
+        try:
+            queryset = queryset.filter(**query_params)
+        except ValidationError:
+            pass
+        if order_by:
+            queryset = queryset.order_by(order_by)
+        if limit:
+            queryset = queryset[:limit]
+        return queryset
+
+
+def pre_view(request, model_name_plural, **kwargs):
+    # TODO: allow for identical model names in different apps
+    models = django.apps.apps.get_models()
+    model, model_name, model_class_name = None, None, None
+    for model_ in models:
+        if model_._meta.model_name == _single_from_plural(model_name_plural):
+            model, model_name, model_class_name = model_, model_._meta.model_name, model_.__name__
+            break
+    if not model:
+        return Response(status_code=404)
+    meta_class = type('Meta', (), {
+        'model': model,
+        'fields': [field.name for field in model._meta.fields]
+    })
+    serializer_class = type(model_class_name + 'Serializer', (HyperlinkedModelSerializer,), {'Meta': meta_class})
+    viewset_class = type(model_class_name + 'ViewSet', (GETparamsViewSet,), {
+        'queryset': model.objects.all(),
+        'serializer_class': serializer_class
+    })
+    if not kwargs.get('pk', None):
+        view = viewset_class.as_view({'get': 'list', 'post': 'create'})
+    else:
+        view = viewset_class.as_view({
+            'get': 'retrieve',
+            'put': 'update',
+            'patch': 'partial_update',
+            'delete': 'destroy',
+        })
+    return_value = view(request, **kwargs)
+    return return_value
