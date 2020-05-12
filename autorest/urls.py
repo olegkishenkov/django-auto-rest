098c6b2d33341864b41bf0c919f4d240d86b7c83
oleg1248 <oleg1248163264@gmail.com>
2020-05-12 13:51:12 +0300

    covered the pre_view wrapper with tests, cases: the REST API exists

--- autorest/urls.py
+++ autorest/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from autorest import views
 
 urlpatterns = [
-    path('<str:model_name_plural>/', views.pre_view),
-    path('<str:model_name_plural>/<int:pk>', views.pre_view),
+    path('<str:model_name_plural>/', views.pre_view, name='model-list'),
+    path('<str:model_name_plural>/<int:pk>', views.pre_view, name='model-detail'),
 ]
